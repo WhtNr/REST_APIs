7158df9fdf95f46be0c159276a63b1fffed79757
melissaperez <melialeja2010@gmail.com>
2023-10-27 09:53:58 -0500

    Agrega parte de seguridad

---
+++ src/main/java/co/edu/unisabana/reservas/reservaciones/config/ApplicationNoSecurity.java
@@ -0,0 +1,18 @@
+package co.edu.unisabana.reservas.Reservaciones.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+
+@Configuration
+@Profile("test")
+
+public class ApplicationNoSecurity {
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring()
+                .dispatcherTypeMatchers(HttpMethod.valueOf("/**"));
+    }
+}
