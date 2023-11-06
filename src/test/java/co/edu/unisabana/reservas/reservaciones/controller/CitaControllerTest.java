731ff6853e7b5e4d1161de81732315c6a9b509a6
brabaez24 <brayanbame@unisabana.edu.co>
2023-11-06 02:22:06 -0500

    Solucion problema entre test y bd

--- src/test/java/co/edu/unisabana/reservas/reservaciones/controller/CitaControllerTest.java
+++ src/test/java/co/edu/unisabana/reservas/reservaciones/controller/CitaControllerTest.java
@@ -29,7 +29,7 @@ class CitaControllerTest {
     @Test
     void givenAvailableSlot_whenProgramarCita_thenSuccess() throws Exception {
 
-        String citaJson = "{\"fecha\":\"2023-10-01\",\"horaInicio\":\"10:00:00\",\"horaFin\":\"11:00:00\"}";
+        String citaJson = "{\"fecha\":\"2023-12-03\",\"horaInicio\":\"10:00:00\",\"horaFin\":\"11:00:00\"}";
 
 
         mockMvc.perform(MockMvcRequestBuilders.post("/api/citas/programar").contentType(MediaType.APPLICATION_JSON).content(citaJson)).andExpect(MockMvcResultMatchers.status().isConflict()).andExpect(MockMvcResultMatchers.content().string("La cita no está disponible."));
@@ -38,7 +38,7 @@ class CitaControllerTest {
     @Test
     void givenExistingCitaId_whenCancelarCita_thenSuccess() throws Exception {
 
-        Long idCita = 7L;
+        Long idCita = 6L;
 
 
         mockMvc.perform(MockMvcRequestBuilders.delete("/api/citas/cancelar/" + idCita)).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.content().string("Cita eliminada con éxito."));
@@ -49,10 +49,10 @@ class CitaControllerTest {
     @Test
     @Transactional
     void programarCita_WithConflict_ShouldReturnConflict() throws Exception {
-        String citaJson = "{\"fecha\":\"2023-10-16\",\"horaInicio\":\"14:30:00\",\"horaFin\":\"15:30:00\"}";
+        String citaJson = "{\"fecha\":\"2023-12-03\",\"horaInicio\":\"14:30:00\",\"horaFin\":\"15:30:00\"}";
 
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/tu-endpoint-aqui/programar-cita").contentType(MediaType.APPLICATION_JSON).content(asJsonString(citaJson))).andExpect(MockMvcResultMatchers.status().isNotFound());
+        mockMvc.perform(MockMvcRequestBuilders.post("/programar-cita").contentType(MediaType.APPLICATION_JSON).content(asJsonString(citaJson))).andExpect(MockMvcResultMatchers.status().isNotFound());
 
     }
 
@@ -60,10 +60,10 @@ class CitaControllerTest {
     @Test
     void givenExistingCitaIdAndReprogramacionRequest_whenReprogramarCita_thenSuccess() throws Exception {
 
-        Long citaId = 8L;
+        Long citaId = 6L;
 
 
-        String reprogramacionJson = "{\"nuevaFecha\":\"2023-10-02\",\"nuevaHoraInicio\":\"15:00:00\",\"nuevaHoraFin\":\"16:00:00\"}";
+        String reprogramacionJson = "{\"nuevaFecha\":\"2023-12-03\",\"nuevaHoraInicio\":\"11:00:00\",\"nuevaHoraFin\":\"12:00:00\"}";
 
 
         mockMvc.perform(MockMvcRequestBuilders.put("/api/citas/reprogramar/" + citaId).contentType(MediaType.APPLICATION_JSON).content(reprogramacionJson)).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.content().string("Cita reprogramada con éxito."));
